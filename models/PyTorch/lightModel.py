bug fixing semi-supervised labeled loss alpha scaling term

models/PyTorch/lightModel.py
@@ -131,19 +131,18 @@ class SimCLR(pl.LightningModule):
 class LitSimCLR(pl.LightningModule):
     # PyTorch Lightning Implementation of SimCLR
     # as manually implemented via A E Foster
-    def __init__(self, net, proj, critic, batch_size, sub_batch_size, lr,
+    def __init__(self, clf, net, proj, critic, batch_size, sub_batch_size, lr,
                  momentum, cosine_anneal, num_epochs, alpha, n_classes,
                  test_freq, testloader):
         super().__init__()
+        # intiialize linear classifier used in validation and testing
+        self.clf = clf
         self.net = net
         self.proj = proj
         self.critic = critic
         self.batch_size = batch_size
         self.sub_batch_size = sub_batch_size
-        self.lr, self.momentum, self.cosine_anneal, self.num_epochs, \
-            self.alpha, self.n_classes, self.test_freq, self.testloader = \
-            lr, momentum, cosine_anneal, num_epochs, alpha, n_classes,
-        test_freq, testloader
+        self.lr, self.momentum, self.cosine_anneal, self.num_epochs, self.alpha, self.n_classes, self.test_freq, self.testloader = lr, momentum, cosine_anneal, num_epochs, alpha, n_classes, test_freq, testloader
         self.save_hyperparameters(ignore=['critic', 'proj', 'net'])
 
     def custom_histogram_adder(self):
@@ -191,15 +190,15 @@ class LitSimCLR(pl.LightningModule):
         # labels1 = latent_clf(representation1)
         # labels2 = latent_clf(representation2)
 
-        # each (x1i, x2i) is a positive pair
-        # if labels is None:
-        # np.unique() avoids repeating supervised class labels
-        classes = np.unique(targets)
-        labels = torch.arange(len(classes)-1,
-                              representation1.shape[0] + len(classes)-1)
         # semi-supervised: define labels for labeled data
-        if np.count_nonzero(targets != -1) > 0:
-            labels[targets != -1] = targets
+        # each (x1i, x2i) is a positive pair
+        labels = targets.detach().clone()
+        # -1 for the unlabeled class in targets
+        # providing a unique label for each unlabeled instance
+        # self.n_classes avoids repeating supervised class labels
+        labels[labels == -1] = torch.arange(self.n_classes,
+                                            len(labels[labels == -1])
+                                            + self.n_classes)
         # sub-batching to preserve memory
         all_losses = []
         for s in range(0, self.batch_size, self.sub_batch_size):
@@ -210,10 +209,18 @@ class LitSimCLR(pl.LightningModule):
             curr_loss = self.critic(curr_emb, curr_labels,
                                     ref_emb=representation2,
                                     ref_labels=labels)
+            print_out = curr_loss['loss']['losses']
             # scaled (only) for supervised contrastive loss term
-            for c in classes:
-                if np.count_nonzero(curr_labels == c) > 0:
-                    curr_loss[curr_labels == c] *= self.alpha
+            # NOTE: if multiple positive samples appear, there will be one loss
+            # for each positive pair (i.e. more than one loss per class).
+            for c in range(self.n_classes):
+                if torch.any(curr_labels == c):
+                    # check for more than one positive pair
+                    indices = torch.where(
+                        torch.isin(curr_loss['loss']['indices'][1],
+                                   torch.where(labels == c)[0]))[0]
+                    # scale losses that match with indices for positive pairs
+                    curr_loss['loss']['losses'][indices] *= self.alpha
             all_losses.append(curr_loss['loss']['losses'])
             # ignore 0 loss when sub_batch is not full
             all_losses = [loss for loss in all_losses

models/SSL/SlimCLRLight.py
@@ -1,8 +1,10 @@
 import argparse
 import os
 import subprocess
+import glob
 
 import torch
+import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import lightning.pytorch as pl
 # from torchlars import LARS
@@ -72,7 +74,7 @@ def parse_arguments():
                         help='base learning rate, rescaled by batch_size/256')
     parser.add_argument("--momentum", default=0.9, type=float,
                         help='SGD momentum')
-    parser.add_argument('--resume', '-r', type=str, default='',
+    parser.add_argument('--resume', '-r', type=str, default=None,
                         help='resume from checkpoint with this filename')
     parser.add_argument('--dataset', '-d', type=str, default='minos',
                         help='dataset keyword',
@@ -130,6 +132,7 @@ def parse_arguments():
 
 
 def main():
+    torch.set_printoptions(profile='full')
     logging.basicConfig(filename='debug.log',
                         filemode='a',
                         level=logging.INFO)
@@ -167,7 +170,7 @@ def main():
         full_trainset = torch.utils.data.ConcatDataset([trainset, ssmlset])
     else:
         full_trainset = trainset
-    trainloader = torch.utils.data.DataLoader(trainset,
+    trainloader = torch.utils.data.DataLoader(full_trainset,
                                               batch_size=args.batch_size,
                                               shuffle=True,
                                               num_workers=args.num_workers,
@@ -175,12 +178,14 @@ def main():
     valloader = torch.utils.data.DataLoader(valset,
                                             batch_size=args.batch_size,
                                             shuffle=False,
-                                            num_workers=args.num_workers,
+                                            # num_workers=args.num_workers,
+                                            num_workers=0,
                                             pin_memory=pin_memory)
     testloader = torch.utils.data.DataLoader(testset,
                                              batch_size=args.batch_size,
                                              shuffle=False,
-                                             num_workers=args.num_workers,
+                                             #  num_workers=args.num_workers,
+                                             num_workers=0,
                                              pin_memory=pin_memory)
 
     # Model
@@ -197,6 +202,7 @@ def main():
     else:
         raise ValueError("Bad architecture specification")
     net = net.to(device)
+    clf = nn.Linear(args.mid[-1], args.n_classes)
     print(f'net dimensions={net.representation_dim}')
 
     ##############################################################
@@ -216,26 +222,33 @@ def main():
         net.representation_dim = repr_dim
         cudnn.benchmark = True
 
-    if args.resume:
-        # Load checkpoint.
-        print('==> Resuming from checkpoint..')
-        assert os.path.isdir('checkpoint'), \
-            'Error: no checkpoint directory found!'
-        resume_from = os.path.join('./checkpoint', args.resume)
-        checkpoint = torch.load(resume_from)
-        net.load_state_dict(checkpoint['net'])
-        critic.load_state_dict(checkpoint['critic'])
+    # if args.resume:
+    #     # Load checkpoint.
+    #     print('==> Resuming from checkpoint..')
+    #     assert os.path.isdir('checkpoint'), \
+    #         'Error: no checkpoint directory found!'
+    #     resume_from = os.path.join('./checkpoint', args.resume)
+    #     checkpoint = torch.load(resume_from)
+    #     net.load_state_dict(checkpoint['net'])
+    #     critic.load_state_dict(checkpoint['critic'])
 
     # make checkpoint directory
     ckpt_path = './checkpoint/'+args.filename+'/'
     if not os.path.isdir(ckpt_path):
         os.mkdir(ckpt_path)
 
+    # if args.resume:
+    #     # the last version run
+    #     last_ver = glob.glob(ckpt_path+'lightning_logs/version_*/')[-1]
+    #     ckpt = ckpt_path + last_ver + glob.glob(last_ver+'checkpoints/*.ckpt')[-1]
+    # else:
+    #     ckpt = None
+
     # save statistical data
     joblib.dump(trainset.mean, ckpt_path+args.filename+'-train_means.joblib')
     joblib.dump(trainset.std, ckpt_path+args.filename+'-train_stds.joblib')
 
-    lightning_model = LitSimCLR(net, proj_head, critic, args.batch_size,
+    lightning_model = LitSimCLR(clf, net, proj_head, critic, args.batch_size,
                                 sub_batch_size, args.lr, args.momentum,
                                 args.cosine_anneal, args.num_epochs,
                                 args.alpha, num_classes, args.test_freq,
@@ -247,7 +260,7 @@ def main():
                          profiler='simple', limit_train_batches=0.75,
                          logger=tb_logger, num_sanity_val_steps=0)
     trainer.fit(model=lightning_model, train_dataloaders=trainloader,
-                val_dataloaders=valloader)
+                val_dataloaders=valloader, ckpt_path=args.resume)
     trainer.test(model=lightning_model, dataloaders=testloader)
 
 